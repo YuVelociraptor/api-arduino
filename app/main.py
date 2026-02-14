a3ebb56a884f64a97a2a58a222ae1a55cdcd68b9
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2026-02-14 21:10:30 +0900

    get&post

--- app/main.py
+++ app/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI with uv (package manager)!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/xxx")
+def post_item():
+    return {"message": "Hello from FastAPI with uv (package manager)!"}
+
